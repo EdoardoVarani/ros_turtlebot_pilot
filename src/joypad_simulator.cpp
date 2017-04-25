8ea8a4f1049fddac8cf25a6fcdffda999f27e12c
EmanueleGhelfi <manughelfi1994@gmail.com>
2017-04-25 12:30:44 -0700

    ncurses working, joypad simulator almost finished

--- src/joypad_simulator.cpp
+++ src/joypad_simulator.cpp
@@ -1,64 +1,116 @@
 #include "joypad_simulator.hh"
-#define KEY_UP 65
-#define KEY_DOWN 66
-#define KEY_RIGHT 67
-#define KEY_LEFT 68
-#define ARROW1 27
-#define ARROW2 91
+#define CTRL_C 3
+#define Q_ASCII 113
 
 void JoyPadSimulator::Prepare()
 {
+    // button map key is the ascii code of the char, value is the position in button array
+    buttonMap[int('a')] = 0;
+    buttonMap[int('s')] = 1;
+    buttonMap[int('d')] = 2;
+    
    // publish on the topic joy
     pub = nodeHandle.advertise<sensor_msgs::Joy>("joy", 10);
 }
 
 void JoyPadSimulator::RunPeriodically()
 {
+    int c = 0;
     setupTerminal();
-    while(ros::ok()){
+    printMenu();
+    while(ros::ok() && c!=Q_ASCII && c!=CTRL_C){
       sensor_msgs::Joy msg;
-      cout << "" << msg.axis.le;
-      int c = getchar();
-      msg.header.stamp = ros::Time().now();
+      // init arrays of axes and buttons
+      msg.axes.push_back(0);
+      msg.axes.push_back(0);
+      msg.axes.push_back(0);
+      msg.axes.push_back(0);
+      
+      std::map<int,int>::iterator mapItr;
+      // reserve a button for each button in button map
+      for(mapItr=buttonMap.begin();mapItr!= buttonMap.end();mapItr++){
+	msg.buttons.push_back(0);
+      }
+      
+      // init header
+      msg.header.stamp= ros::Time().now();
+      
+      // get character
+      c = getch();
+      
+      // switch on arrow
       switch (c){
 	case KEY_LEFT:
 	  msg.axes[3] = -1;
+	  printw("Angular velocity left\n");
 	  break;
 	case KEY_RIGHT:
-	   msg.axes[3] = -1;
+	   msg.axes[3] = 1;
+	    printw("Angular velocity right\n");
 	   break;
 	case KEY_UP:
+	   printw("Linear velocity forward\n");
 	   msg.axes[1] = 1;
 	   break;
 	case KEY_DOWN:
+	   printw("Linear velocity backward\n");
 	   msg.axes[1] = -1;
 	   break;
+	  };
+	
+      
+      // try get a button
+      try{
+	msg.buttons.at(buttonMap.at(c))=1;
+      }catch (const std::out_of_range& oor) {
+	
       };
+      
+      // print
+      refresh();
+      
+      // publish message
       pub.publish(msg);
       
     }
     
+   // restore terminal normal mode
     restoreTerminal();
+    
+    // print ctrl c
+    if(c==CTRL_C || c==Q_ASCII){
+      std::cout <<" Quit \n\n";
+    }
 }
 
 void JoyPadSimulator::setupTerminal(){
-  // tcgetattr gets parameters of the current terminal
-  // STDIN_FILENO will tell tcgetattr that it should write the setting of stdin to oldt
-  tcgetattr(STDIN_FILENO, &this->oldt);
+  // init functions
+  initscr();
+  // get functions and arrow
+  keypad(stdscr,true);
+  // do not wait enter
+  raw();
+  //cbreak();
+  // do not print characters on terminal
+  noecho();
   
-  // copy settings
-  this->newt = this->oldt;
-  
-  // disable echo and end set raw mode
-  this->newt.c_lflag &= ~(ICANON | ECHO);
-  
-  // set new settings to STDIN TCSANOW tells tcsetattr to change attributes now
-  tcsetattr(STDIN_FILENO,TCSANOW, &this->newt);
 }
 
+
+
 void JoyPadSimulator::restoreTerminal(){
-    tcsetattr(STDIN_FILENO,TCSANOW,&this->oldt);
+  // reset terminal normal mode
+   endwin();
 }
+
+void JoyPadSimulator::printMenu()
+{
+  printw("Welcome to JoyPad Simulator node\n");
+  printw("Supported Keys:\n");
+  printw("\t-\tLeft, Right, Up, Down arrows for velocities\n");
+  printw("\t-\t a,s,d buttons\n\n");
+}
+
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "joypad_simulator");
--- src/joypad_simulator.hh
+++ src/joypad_simulator.hh
@@ -9,22 +9,24 @@
 #include <unistd.h>
 #include <ncurses.h>
 #include <cstdlib>
-
+#include <map>
 
 class JoyPadSimulator{
 private: ros::NodeHandle nodeHandle;
 	ros::Publisher pub;
 	sensor_msgs::Joy msg_out;
 	struct termios oldt,newt;
+	std::map<int,int> buttonMap;
+	void printMenu();
+	///\brief setup terminal in raw mode disabling echo
+	void setupTerminal();
+	///\brief restore old settings
+	void restoreTerminal();
 
 	
 public: 
   void Prepare();
   void RunPeriodically();
-  ///\brief setup terminal in raw mode disabling echo
-  void setupTerminal();
-  ///\brief restore old settings
-  void restoreTerminal();
   
 };
 
